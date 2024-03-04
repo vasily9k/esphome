a477ed192ce7754d429c564dfa269f73da4f3b0c
Vasily9k-ft <vasily9k@gmail.com>
2024-03-05 00:58:26 +0800

    demo22_demosensor

--- esphome/components/demo22/__init__.py
+++ esphome/components/demo22/__init__.py
@@ -74,8 +74,8 @@ CONFIG_SCHEMA = cv.Schema(
 
 
 async def to_code(config):
-    var = cg.new_Pvariable(config[CONF_ID])
-    await cg.register_component(var, config)
+    #var = cg.new_Pvariable(config[CONF_ID])
+    #await cg.register_component(var, config)
     
     for conf in config[CONF_SENSORS]:
         var = await sensor.new_sensor(conf)
